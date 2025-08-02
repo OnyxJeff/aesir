6d8033943d55b35a61a3ae834bafaa487f42dd66
OnyxJeff <j.may2702@gmail.com>
2025-08-02 00:13:23 -0400

    custom build.func; added logo

--- scripts/lxc-sindri.sh
+++ scripts/lxc-sindri.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/OnyxJeff/aesir/main/misc/build.func)
 
 # App Metadata
 APP="Sindri"
